4bb971ae3aae2570a863e2ee40f293044ca1f486
EmilyPlatzer <emilyplatzer@gmail.com>
2014-11-25 13:29:06 -0700

    Posts are dynamically listed from the model

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ App.Router.map(function() {
   this.resource('about');
   this.resource('posts');
 });
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+var posts = [{
+  id: '1',
+  title: 'Intro to Ember',
+  author: { name: 'Emily Platzer' },
+  date: new Date('11-24-2014'),
+  excerpt: "I'm really excited to get into Ember.",
+  body: "Words and words and words and words. Here is the part where I'm coding, and here is the part where I'm figuring stuff out, and here is the part where I'm learning. Words and words and words and words. Here is the part where I'm coding, and here is the part where I'm figuring stuff out, and here is the part where I'm learning. Words and words and words and words. Here is the part where I'm coding, and here is the part where I'm figuring stuff out, and here is the part where I'm learning. Words and words and words and words. Here is the part where I'm coding, and here is the part where I'm figuring stuff out, and here is the part where I'm learning.",
+}, {
+  id: '2',
+  title: 'Ember Continued',
+  author: { name: 'Emily Platzer' },
+  date: new Date('11-26-2014'),
+  excerpt: "Ember is pretty fun!",
+  body: "I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!! I am working in Ember and making stuff! Pretty exciting. Check it out!!!",
+}];
